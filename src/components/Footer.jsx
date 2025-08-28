5a0fd690c41d5b963097cca97f13f7dd0be7fb63
Subhajit Seth <121784637+Subhajit-Seth@users.noreply.github.com>
2025-08-28 14:20:12 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,9 @@ export default function Footer() {
     return (
         <footer>
             <div className="container">
-                <p>&copy; 2025 Real Bharat News</p>
+                <p>&copy; 2025 Real Bharat News. Developed by Subhajit Seth.</p>
             </div>
         </footer>
     );
+
 }
